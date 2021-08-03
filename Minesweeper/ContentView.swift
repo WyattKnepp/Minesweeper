1bef4e0097c6d4dbf7f5c8da7ebd2325c8c2ea9c
Wyatt Knepp <wyattjoeknepp@gmail.com>
2021-08-03 12:34:25 -0500

    Built cell struct base

--- Minesweeper/ContentView.swift
+++ Minesweeper/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+	
+	@State private var boardX = 8
+	@State private var boardY = 8
+	var body: some View {
+		
+		Text("Placeholder")
+		
+//		VStack {
+//			for i in boardY{
+//				HStack{
+//					for j in boardX {
+//						Text("\(i * j)")
+//					}
+//				}
+//			}
+//		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
+}
+
+struct Cell: View {
+	var isFlagged = false
+	var isRevealed = false
+	var numOfMines = 0
+	var isMine = false
+	
+	var body: some View {
+		ZStack{
+			RoundedRectangle(cornerRadius: 25.0)
+			//Image()
+		}
+	}
 }
