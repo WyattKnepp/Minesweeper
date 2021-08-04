b98e4d0376cdb4d7d5da651995b632849c09bbf4
Wyatt Knepp <wyattjoeknepp@gmail.com>
2021-08-03 22:00:31 -0500

    Created cells and board

--- Minesweeper/ContentView.swift
+++ Minesweeper/ContentView.swift
@@ -9,40 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
 	
-	@State private var boardX = 8
-	@State private var boardY = 8
+	let boardX = 8
+	let boardY = 8
 	var body: some View {
 		
-		Text("Placeholder")
-		
-//		VStack {
-//			for i in boardY{
-//				HStack{
-//					for j in boardX {
-//						Text("\(i * j)")
-//					}
-//				}
-//			}
-//		}
+		VStack {
+			ForEach(0..<boardX) { i in
+				HStack {
+					ForEach(0..<boardY) { j in
+						Cell(xPos: i, yPos: j)
+					}
+				}
+			}
+		}
 	}
-}
-
-struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		ContentView()
-	}
-}
-
-struct Cell: View {
-	var isFlagged = false
-	var isRevealed = false
-	var numOfMines = 0
-	var isMine = false
 	
-	var body: some View {
-		ZStack{
-			RoundedRectangle(cornerRadius: 25.0)
-			//Image()
+	struct ContentView_Previews: PreviewProvider {
+		static var previews: some View {
+			ContentView()
+		}
+	}
+	
+	struct Cell: View {
+		var isFlagged = false
+		var isRevealed = false
+		var numOfMines = 0
+		var isMine = false
+		
+		var xPos : Int
+		var yPos : Int
+		
+		var body: some View {
+			ZStack{
+				RoundedRectangle(cornerRadius: 15.0)
+					.frame(width: 30.0, height: 30.0)
+				Text("\((xPos*8) + (yPos))")
+					.foregroundColor(Color.white)
+					.multilineTextAlignment(.center)
+					.padding(0.0)
+					
+			}
 		}
 	}
 }
